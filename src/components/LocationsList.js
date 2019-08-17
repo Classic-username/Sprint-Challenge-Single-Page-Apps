cdcd20e8c3f7ea9413289a3cf3f3a43e5e4b73aa
Classic-username <josh.drapergame@gmail.com>
2019-08-16 19:54:20 -0600

    axiosd and displayed data for locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,51 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import styled from 'styled-components';
 
-export default function LocationsList() {}
+export default function LocationsList() {
+    const [ loc, setLoc ] = useState([])
+
+    const LocDiv = styled.div`
+        
+        width: 95%;
+        display: flex;
+        
+        height: 100%;
+
+    `;
+    const InLocDiv = styled.div`
+        width: 40%;
+        border: 2px solid black;
+
+    `;
+
+    useEffect(() => {
+        Axios
+            .get('https://rickandmortyapi.com/api/location/')
+            .then(res => {
+                console.log(res.data.results);
+                setLoc(res.data.results);
+
+            })
+            .catch(deny => {
+                console.log(deny)
+            })
+    }, [])
+
+    return(
+        <LocDiv>
+            ${loc.map(cv => {
+                return(
+                    <InLocDiv key={cv.id}>
+                        <h2>{cv.name}</h2>
+                        <p>{cv.type} - {cv.dimension}</p>
+                        <div>{cv.residents.length} Residents</div>
+                    </InLocDiv>
+                )
+            })}
+        </LocDiv>
+    )
+    
+    
+    
+}
