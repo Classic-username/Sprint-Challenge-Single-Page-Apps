9a2fddff22eb45af32b2e39a8e7a13e4f69a0a50
Classic-username <josh.drapergame@gmail.com>
2019-08-16 21:59:49 -0600

    yup installed

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function SearchForm({ onSearch }) {
   // TODO: Add stateful logic for query/form data
   const [ name, setName ] = useState('')
 
-  
+
   return (
     <section className="search-form">
       <form onSubmit={() => onSearch(name)}>
