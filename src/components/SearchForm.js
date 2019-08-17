4d67791c2d19dbd666ee585f372f33758b84f874
Classic-username <josh.drapergame@gmail.com>
2019-08-16 22:22:26 -0600

    Got as far as I could. did not complete searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,23 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import Axios from "axios";
 
-export default function SearchForm({ onSearch }) {
+const SearchFormK = ({ errors, touched, values, status }) => {
   // TODO: Add stateful logic for query/form data
   const [ name, setName ] = useState('')
 
+  useEffect(() => {
+    if(status) {
+      setName(...name, status)
+    }
+  }, [status])
+
+  //onSubmit={() => onSearch(name)}
 
   return (
     <section className="search-form">
-      <form onSubmit={() => onSearch(name)}>
-        <input
+      <Form >
+        <Field 
+          component='input' 
+          type='text' 
+          name='name' 
+          placeholder='name'
+          values={name}/>
+        {/* <input
           onChange={handleInputChange}
           placeholder="name"
           value={name}
           name="name"
-        />
+        /> */}
         <button type="submit">Search</button>
-      </form>
+      </Form>
     </section>
   );
-}
+};
+
+const formikHOC = withFormik({
+  mapPropsToValues({ name }) {
+    return{
+      name: name || ''
+    }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('please enter a name')
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    Axios
+      .get('https://rickandmortyapi.com/api/character/', values)
+      .then(res => {
+        setStatus(res.data);
+        resetForm();
+      })
+      .catch(err => console.error(err))
+  }
+})
+
+const SearchForm = formikHOC(SearchFormK)
+
+
+export default SearchForm
